73d1e93cb75629fd7c91b4f20b25ed36b48e860b
zainul <zainulmasadi90@gmail.com>
2016-09-20 15:12:18 +0700

    adding read model from generated import

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 var SequelizeAuto = require('sequelize-auto')
 var transform = require('./generic_transform')
+var Sequelize = require('sequelize');
 var fs = require('fs');
 var path = require("path");
 var parse = require('csv-parse');
 var async = require('async');
 var files = './files/';
 
+var sequelize = new Sequelize('healthcare', 'zainul', 'Realtime1!', {
+    dialect: 'postgres',
+    port: 5432,
+    host: 'localhost',
+    define: {
+      timestamps: false
+    }
+});
+
+// 1 & 2
 // var auto = new SequelizeAuto('healthcare', 'zainul', 'Realtime1!', {
 //     dialect: 'postgres',
 //     port: 5432,
 //     host: 'localhost'
 // });
 
-var file = 'contacts.csv';
+var file = String("contacts.csv");
+var model = file.split('.')[0];
 
-async.waterfall([
-    async.apply(fs.readFile, path.join(__dirname, files, file)),
-    parse,
-    transform.readRecords,
-], (results, err) => {
-  console.log(results)
-});
+// 3
+// async.waterfall([
+//     async.apply(fs.readFile, path.join(__dirname, files, file)),
+//     parse,
+//     transform.readRecords,
+// ], (results, err) => {
+//   console.log(results)
+// });
 
+// 4
+// console.log(model)
+// var Model = sequelize.import(__dirname + "/models/" + model);
+// Model.findOne({ where: 1 }).then((sqlResponse)=> {
+//   console.log(sqlResponse.toJSON());
+// })
+// .catch(function (error) {
+//   // console.log({ error })
+// });
+
+// 2
 // auto.run(function(err) {
 //     if (err) throw err;
 //
